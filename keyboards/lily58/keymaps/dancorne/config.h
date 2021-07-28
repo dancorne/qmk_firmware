8485865dfd664f938656af22a9f54ea40362009c
Dan Corne <dan.corne@freeagent.com>
2021-07-28 13:56:50 +0100

    More dancorne changes

--- keyboards/lily58/keymaps/dancorne/config.h
+++ keyboards/lily58/keymaps/dancorne/config.h
@@ -20,20 +20,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-//#define USE_MATRIX_I2C
-
 /* Select hand configuration */
 
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-// #define SSD1306OLED
-
-#define USE_SERIAL_PD2
+//#define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
+//#define PERMISSIVE_HOLD
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
