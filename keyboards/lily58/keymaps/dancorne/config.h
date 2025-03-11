867bd739f12651d1ac75a1cf5335e0f247d5d441
Dan Corne <dan.corne@freeagent.com>
2025-03-11 13:25:45 +0000

    Latest keymap
    I've been using this for a couple of months and it's working really well. Only things I might change are:
     - There's currently three shift keys, should I rely on the thumb or pinkies?
     - Maybe space should be on the right thumb, next to Enter? Lots of other keyboards do it that way round.
     - Maybe shift the symbols layer round a bit -- I use some of them, and some I just use the default layer.

--- keyboards/lily58/keymaps/dancorne/config.h
+++ keyboards/lily58/keymaps/dancorne/config.h
@@ -30,7 +30,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
-//#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
+#define PERMISSIVE_HOLD
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
